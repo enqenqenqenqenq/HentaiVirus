98f047e98e40c9ad29ff38a02e4ca66e58de8bb3
zenkindaniil86-tech <zenkindaniil86@gmail.com>
2026-05-25 22:06:40 +0300

    Создание базового класса Downloader

---
+++ HentaiVirus/Core/Downloader.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HentaiVirus.Core
+{
+    public class Downloader
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public Downloader()
+        {
+            // Настройка заголовков для имитации обычного браузера
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+        }
+
+        public async Task DownloadArchiveAsync(string url, string destinationPath)
+        {
+            // TODO: Реализация скачивания байтового потока и сохранения на диск
+            await Task.CompletedTask;
+        }
+    }
+}
